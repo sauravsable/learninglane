b8439a6bfed8fbb86680fea286ce0dd95f14e150
sauravsable <sauravsable4102@gmail.com>
2024-01-27 00:00:54 +0530

    initial commit

---
+++ src/components/services.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Our Services</h2>
+          <h3>Make learninglane work for your organizations!</h3>
+          <p>
+          We offer a range of learning solutions that will help your organization to create your own high-quality digital learning environment by using open educational resources (OER).
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
